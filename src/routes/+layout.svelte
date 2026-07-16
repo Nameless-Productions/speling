2572f6cefe886c5d551e1f4d7ad322170f1ecdc5
thenamelessdev <aron.minarovits@gmail.com>
2026-07-16 14:10:18 +0200

    feat: make navbar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,14 @@
 <script lang="ts">
+	import NavBar from '../lib/componets/NavBar.svelte';
 	import './layout.css';
 
-	let { children } = $props();
+	let { children, data } = $props();
 </script>
 
 <svelte:head><title>Speling</title></svelte:head>
 
+
+<NavBar user={data.user} />
 
 <main class="m-2">
 	{@render children()}
