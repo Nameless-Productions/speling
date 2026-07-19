e6283845d52cac4617769e3c6ce815ca2a56cb66
thenamelessdev <aron.minarovits@gmail.com>
2026-07-19 19:29:23 +0200

    chore: format things

--- src/routes/post/+page.server.ts
+++ src/routes/post/+page.server.ts
@@ -1,48 +1,49 @@
-import { zod4 } from "sveltekit-superforms/adapters";
-import type { Actions, PageServerLoad } from "./$types";
-import { fail, superValidate } from "sveltekit-superforms"
-import { postSchema } from "$lib/types/postSchema";
-import { redirect } from "@sveltejs/kit";
-import Typo from "typo-js"
-import { db } from "$lib/db";
+import { zod4 } from 'sveltekit-superforms/adapters';
+import type { Actions, PageServerLoad } from './$types';
+import { fail, superValidate } from 'sveltekit-superforms';
+import { postSchema } from '$lib/types/postSchema';
+import { redirect } from '@sveltejs/kit';
+import Typo from 'typo-js';
+import { db } from '$lib/db';
 
 export const load: PageServerLoad = async () => {
-    const form = await superValidate(zod4(postSchema));
+	const form = await superValidate(zod4(postSchema));
 
-    return { form }
-}
-
+	return { form };
+};
 
 export const actions: Actions = {
-    post: async ({request, locals}) => {
-        if (!locals.User) return redirect(303, new URL("/login", request.url));
+	post: async ({ request, locals }) => {
+		if (!locals.User) return redirect(303, new URL('/login', request.url));
 
-        const form = await superValidate(request, zod4(postSchema));
-        if (!form.valid) return fail(400, {form});
+		const form = await superValidate(request, zod4(postSchema));
+		if (!form.valid) return fail(400, { form });
 
-        const dictionary = new Typo("en_US");   
-        //                                            removes all punctuation marks
-        const contentArr = form.data.content.replace(/[!.,;:?"'()[\]{}<>/\\\-_@#$%^&*+=~`|]/g, " ").split(" ");
-        let typos = 0;
+		const dictionary = new Typo('en_US');
+		//                                            removes all punctuation marks
+		const contentArr = form.data.content
+			.replace(/[!.,;:?"'()[\]{}<>/\\\-_@#$%^&*+=~`|]/g, ' ')
+			.split(' ');
+		let typos = 0;
 
-        for (const word of contentArr) {
-            if (dictionary.check(word)) {
-                continue
-            } else {
-                typos += 1;
-            }
-        };
+		for (const word of contentArr) {
+			if (dictionary.check(word)) {
+				continue;
+			} else {
+				typos += 1;
+			}
+		}
 
-        const post = await db.post.create({
-            data: {
-                authorID: locals.User.id,
-                typoCount: typos,
-                likes: 0,
-                content: form.data.content,
-                createdAt: new Date()
-            }
-        });
+		const post = await db.post.create({
+			data: {
+				authorID: locals.User.id,
+				typoCount: typos,
+				likes: 0,
+				content: form.data.content,
+				createdAt: new Date()
+			}
+		});
 
-        return redirect(303, new URL(`/post/${post.id}`, request.url))
-    }
-}
+		return redirect(303, new URL(`/post/${post.id}`, request.url));
+	}
+};
--- src/routes/post/+page.svelte
+++ src/routes/post/+page.svelte
@@ -1,22 +1,31 @@
 <script lang="ts">
 	import { superForm } from 'sveltekit-superforms';
 
-    const {data} = $props()
-    
-    const { form, errors, enhance, submitting } = superForm(data.form);
+	const { data } = $props();
+
+	const { form, errors, enhance, submitting } = superForm(data.form);
 </script>
 
 <div class="flex h-screen items-center justify-center">
-    <div class="border-2 border-white rounded-xl p-5">
-        <form action="?/post" method="post" use:enhance>
-            <textarea name="content" placeholder="What's on your mind?" bind:value={$form.content} class="border border-gray-500 p-1 rounded-xl w-100 h-50" required></textarea>
-            {#if $errors.content}
-                <p class="text-red-500">{$errors.content}</p>
-            {/if}
+	<div class="rounded-xl border-2 border-white p-5">
+		<form action="?/post" method="post" use:enhance>
+			<textarea
+				name="content"
+				placeholder="What's on your mind?"
+				bind:value={$form.content}
+				class="h-50 w-100 rounded-xl border border-gray-500 p-1"
+				required></textarea>
+			{#if $errors.content}
+				<p class="text-red-500">{$errors.content}</p>
+			{/if}
 
-            <br>
-            
-            <button type="submit" class="border mt-3 rounded-full w-full cursor-pointer bg-neutral-800 hover:bg-neutral-600 duration-300" disabled={$submitting}>{$submitting ? "Sending..." : "Post"}</button>
-        </form>
-    </div>
+			<br />
+
+			<button
+				type="submit"
+				class="mt-3 w-full cursor-pointer rounded-full border bg-neutral-800 duration-300 hover:bg-neutral-600"
+				disabled={$submitting}>{$submitting ? 'Sending...' : 'Post'}</button
+			>
+		</form>
+	</div>
 </div>
