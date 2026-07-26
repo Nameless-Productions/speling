24af3ca8247e3629908052e23ee4b9af01245a0d
thenamelessdev <nameless@thenamelessdev.com>
2026-07-26 13:59:41 +0200

    feat(feed): frontend for feed page

---
+++ src/routes/feed/+page.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+    let {data} = $props();
+</script>
+
+<div class="flex flex-col items-center">
+    {#each data.posts as post (post.date)}
+        <div class="border-b border-t border-b-white border-t-white max-w-sm w-full p-2 mx-auto">
+            <p class="font-bold">{post.author}</p>
+            <p>{post.content}</p>
+
+            <div class="flex">
+                <p class="p-0.5 bg-blue-600 rounded-l-full my-2 w-1/2">Typos: {post.typos}</p>
+                <p class="p-0.5 bg-red-600 rounded-r-full my-2 w-1/2">Likes: {post.likes}</p>
+            </div>
+        </div>
+    {/each}
+</div>
