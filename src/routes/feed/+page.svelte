a5032e789ac0de3d6413036c9c4199dfcfee596d
thenamelessdev <nameless@thenamelessdev.com>
2026-07-26 22:24:11 +0200

    feat(feed): implement new api endpoint to feed page

--- src/routes/feed/+page.svelte
+++ src/routes/feed/+page.svelte
@@ -1,30 +1,32 @@
 <script lang="ts">
 	import Post from '$lib/componets/Post.svelte';
+	import type { Post as PostType } from '$lib/types/post';
+	import { onMount } from 'svelte';
 
-	let { data } = $props();
-	let posts = $state(data.posts);
-	let typosFirst = $state(false);
+	let posts = $state<PostType[]>([]);
+	let typosFirst = $state<"false" | "true">("true");
+	let page = $state(0);
+
+	onMount(async () => {
+		const res = await fetch(`/api/posts?typosFirst=${typosFirst}&page=${page}`);
+		if (!res.ok) return;
+
+		const resJson = await res.json() as PostType[];
+
+		posts = resJson;
+	})
 
 	$effect(() => {
-		if (typeof window !== 'undefined') {
-			typosFirst = new URL(window.location.href).searchParams.get('typosFirst') === 'true';
-		}
-	});
+		(async () => {
+			const res = await fetch(`/api/posts?typosFirst=${typosFirst}&page=${page}`);
+			if (!res.ok) return;
 
-	const sortedPosts = $derived(
-		typosFirst ? [...posts].sort((a, b) => (b.typos ?? 0) - (a.typos ?? 0)) : posts
-	);
+			const resJson = await res.json() as PostType[];
 
-	function handleFilterChange(value: string) {
-		const url = new URL(window.location.href);
-		if (value === 'typosFirst') {
-			url.searchParams.set('typosFirst', 'true');
-		} else {
-			url.searchParams.delete('typosFirst');
-		}
-		window.history.replaceState({}, '', url.toString().split(window.location.origin)[1]);
-		typosFirst = value === 'typosFirst';
-	}
+			posts = resJson;
+		})()
+	})
+
 </script>
 
 <div class="flex flex-col items-center">
@@ -32,16 +34,15 @@
 		<label for="sort" class="mb-2 block text-sm font-medium">Sort by:</label>
 		<select
 			id="sort"
-			onchange={(e) => handleFilterChange(e.currentTarget.value)}
-			value={typosFirst ? 'typosFirst' : 'default'}
+			bind:value={typosFirst}
 			class="w-full rounded-md border border-gray-300 px-3 py-2"
 		>
-			<option value="default">Default</option>
-			<option value="typosFirst">Typos First</option>
+			<option value="false">Default</option>
+			<option value="true">Typos First</option>
 		</select>
 	</div>
 
-	{#each sortedPosts as post (post.date)}
+	{#each posts as post (post.date)}
 		<div class="mx-auto w-full max-w-sm border-t border-b border-t-white border-b-white p-2">
 			<Post {post} />
 		</div>
