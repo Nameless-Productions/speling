ad8a8ddcccff29c8265db8f8b49750097c973cc5
thenamelessdev <nameless@thenamelessdev.com>
2026-07-26 12:54:37 +0200

    feat: frontend for leaderboard

---
+++ src/routes/leaderboard/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    let {data} = $props()
+</script>
+
+<ul class="list-decimal list-inside">
+    {#each data.users as user (user.id)}
+       <li class="flex">{user.username}<span class="ml-auto">Typos: {user.typoCount}</span></li> 
+    {/each}
+</ul>
