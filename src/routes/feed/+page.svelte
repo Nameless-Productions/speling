507b5b40fd84b2b5310573c31fef0edb8ee02ec1
thenamelessdev <nameless@thenamelessdev.com>
2026-07-26 14:19:07 +0200

    feat(feed): ability to like a post

--- src/routes/feed/+page.svelte
+++ src/routes/feed/+page.svelte
@@ -1,5 +1,20 @@
 <script lang="ts">
     let {data} = $props();
+
+    async function like(postID: number) {
+        const res = await fetch(`/api/like?post=${postID}`);
+        if (!res.ok) return;
+
+        const resJson = await res.json() as {likes: number};
+
+        let i = 0;
+        for (const post of data.posts) {
+            if (post.id === postID) {
+                data.posts[i].likes = resJson.likes
+            }
+            i += 1;
+        }
+    }
 </script>
 
 <div class="flex flex-col items-center">
@@ -10,7 +25,7 @@
 
             <div class="flex">
                 <p class="p-0.5 bg-blue-600 rounded-l-full my-2 w-1/2 text-center">Typos: {post.typos}</p>
-                <p class="p-0.5 bg-red-600 rounded-r-full my-2 w-1/2 text-center">Likes: {post.likes}</p>
+                <button type="button" onclick={() => like(post.id)} class="p-0.5 bg-red-600 hover:bg-red-500 duration-300 cursor-pointer rounded-r-full my-2 w-1/2 text-center">Likes: {post.likes}</button>
             </div>
         </div>
     {/each}
