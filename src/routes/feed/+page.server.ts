789c57e5a16bb7b62fc52d7bbf72cf68c8520746
thenamelessdev <nameless@thenamelessdev.com>
2026-07-26 13:41:14 +0200

    feat(feed): return comments

--- src/routes/feed/+page.server.ts
+++ src/routes/feed/+page.server.ts
@@ -4,7 +4,7 @@ import type { PageServerLoad } from "./$types";
 export const load: PageServerLoad = async ({url}) => {
     const typosFirst = url.searchParams.get("typosFirst") === "true" ? true : false;
 
-    const posts: {author: string, content: string, likes: number, date: Date}[] = []
+    const posts: {author: string, content: string, likes: number, date: Date, comments: {author: string, content: string}[]}[] = []
 
     const postsDB = await db.post.findMany({
         take: 100,
@@ -28,7 +28,26 @@ export const load: PageServerLoad = async ({url}) => {
         });
         if (!user) continue;
 
-        posts.push({author: user.username, content: post.content, likes: likes.length, date: post.createdAt})
+        const commentsDB = await db.comment.findMany({
+            where: {
+                post: post.id
+            }
+        })
+
+        const comments: {author: string, content: string}[] = [];
+
+        for (const comment of commentsDB) {
+            const author = await db.user.findUnique({
+                where: {
+                    id: comment.authorID
+                }
+            });
+            if (!author) continue;
+
+            comments.push({author: author.username, content: comment.content})
+        }
+
+        posts.push({author: user.username, content: post.content, likes: likes.length, date: post.createdAt, comments})
     }
 
     return {posts}
