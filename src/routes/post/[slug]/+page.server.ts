3c9c47a7848c88090ee343b0676959edc05b1c53
thenamelessdev <nameless@thenamelessdev.com>
2026-07-26 11:58:34 +0200

    feat: ability to view posts

---
+++ src/routes/post/[slug]/+page.server.ts
@@ -0,0 +1,24 @@
+import { error } from "@sveltejs/kit";
+import type { PageServerLoad } from "./$types";
+import { db } from "$lib/db";
+
+export const load: PageServerLoad = async ({params}) => {
+    const id = Number(params.slug);
+    if (isNaN(id)) return error(400, "ID is NaN");
+
+    const post = await db.post.findUnique({
+        where: {
+            id
+        }
+    });
+    if (!post) return error(404, "Post not found");
+
+    const author = await db.user.findUnique({
+        where: {
+            id: post.authorID
+        }
+    });
+    if (!author) return error(404, "Post author not found")
+
+    return {post, author}
+}
---
+++ src/routes/post/[slug]/+page.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    let { data } = $props();
+</script>
+
+<div class="flex h-screen items-center justify-center">
+    <div class="border-2 border-gray-600 rounded-xl p-3">
+        <p class="font-bold">{data.author.username}</p>
+        <br>
+        <p>{data.post.content}</p>
+        <br>
+        <p class="p-0.5 rounded-full font-bold w-full text-center bg-blue-500">Typos: {data.post.typoCount}</p>
+    </div>
+</div>
