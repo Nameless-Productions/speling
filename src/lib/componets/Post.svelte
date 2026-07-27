17fa7a8e2e1ddcd9a2d8a3569376998f2cd11b1b
thenamelessdev <nameless@thenamelessdev.com>
2026-07-27 14:40:22 +0200

    feat(post component): comment count

--- src/lib/componets/Post.svelte
+++ src/lib/componets/Post.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import { resolve } from '$app/paths';
 	import type { Post } from '$lib/types/post';
 
 	let { post }: { post: Post } = $props();
@@ -16,12 +17,17 @@
 	<p>{post.content}</p>
 
 	<div class="flex">
-		<p class="my-2 w-1/2 rounded-l-full bg-blue-600 p-0.5 text-center">Typos: {post.typos}</p>
+		<p class="my-2 w-1/3 rounded-l-full bg-blue-600 p-0.5 text-center">Typos: {post.typos}</p>
 		<button
 			type="button"
 			onclick={() => like()}
-			class="my-2 w-1/2 cursor-pointer rounded-r-full bg-red-600 p-0.5 text-center duration-300 hover:bg-red-500"
+			class="my-2 w-1/3 cursor-pointer bg-red-600 p-0.5 text-center duration-300 hover:bg-red-500"
 			>Likes: {post.likes}</button
+		>
+		<a
+			href={resolve(`/post/${post.id}`)}
+			class="my-2 w-1/3 cursor-pointer rounded-r-full bg-gray-600 p-0.5 text-center duration-300 hover:bg-gray-500"
+			>Comments: {post.commentCount}</a
 		>
 	</div>
 </div>
