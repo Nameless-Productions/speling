21ca8188a9e7aa1982b61a87c1e27070ed12c143
thenamelessdev <nameless@thenamelessdev.com>
2026-07-28 20:43:10 +0200

    feat(profile func): make a get profile function that gets a profile with the first 50 posts

---
+++ src/lib/getProfile.ts
@@ -0,0 +1,47 @@
+import { db } from './db';
+import type { Post } from './types/post';
+
+export async function getProfile(id: number) {
+	const user = await db.user.findUnique({
+		where: {
+			id
+		}
+	});
+	if (!user) return;
+
+	const postsDB = await db.post.findMany({
+		take: 50,
+		where: {
+			authorID: user.id
+		}
+	});
+
+	const posts: Post[] = [];
+
+	for (const post of postsDB) {
+		const likes = await db.like.findMany({
+			where: {
+				postID: post.id
+			}
+		});
+
+		const comments = await db.comment.findMany({
+			where: {
+				post: post.id
+			}
+		});
+
+		posts.push({
+			author: user.username,
+			content: post.content,
+			likes: likes.length,
+			date: post.createdAt,
+			comments: [],
+			typos: post.typoCount,
+			id: post.id,
+			commentCount: comments.length
+		});
+	}
+
+	return { user, posts };
+}
