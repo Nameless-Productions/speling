2bcadbcf7370472bd82b8f9ab0225327b1486019
thenamelessdev <nameless@thenamelessdev.com>
2026-07-29 12:28:24 +0200

    feat(S3 client): make a s3 client and export it

---
+++ src/lib/client.ts
@@ -0,0 +1,11 @@
+import { CF_ACCESS_KEY, CF_SECRET_KEY, CF_URL } from '$env/static/private';
+import { S3Client } from '@aws-sdk/client-s3';
+
+export const s3 = new S3Client({
+	region: 'auto',
+	endpoint: CF_URL,
+	credentials: {
+		accessKeyId: CF_ACCESS_KEY,
+		secretAccessKey: CF_SECRET_KEY
+	}
+});
