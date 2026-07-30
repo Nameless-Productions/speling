0f48255f8f32e7a2a1d9f854976b65b62ee618d2
thenamelessdev <nameless@thenamelessdev.com>
2026-07-30 20:31:54 +0200

    feat(user page): delete account button with dialog

---
+++ src/lib/componets/DeleteUserPopup.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import { resolve } from "$app/paths";
+
+    let { userID }: { userID: number } = $props();
+</script>
+
+<dialog class="bg-neutral-500 p-5 rounded-xl text-neutral-300 m-auto" open>
+    <p>Are you sure that you want to delete your user?</p>
+    <p>This will delete all of your:</p>
+    <ul class="list-disc list-inside ml-4">
+        <li>Posts</li>
+        <li>Uploaded images</li>
+        <li>Comments</li>
+        <li>Likes</li>
+    </ul>
+    <p>This cannot be reversed</p>
+    <p>You can recreate your account again by logging in with Discord <br>but all of your data will be deleted</p>
+
+    <button type="button" onclick={() => location.reload()} class="p-1 bg-green-600 rounded-xl cursor-pointer hover:bg-green-400 duration-300">Go Back</button>
+    <a href={resolve(`/user/${userID}/delete`)} class="p-1 bg-red-600 rounded-xl cursor-pointer hover:bg-red-400 duration-300">Delete account</a>
+</dialog>
--- src/routes/user/[slug]/+page.svelte
+++ src/routes/user/[slug]/+page.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
+	import DeleteUserPopup from '$lib/componets/DeleteUserPopup.svelte';
 	import Post from '$lib/componets/Post.svelte';
 
 	const { data } = $props();
+	let isDeleteOpen = $state(false);
 </script>
 
 <div class="flex flex-col items-center">
@@ -9,7 +11,10 @@
 		- You
 	{/if}</p>
 
-	<p class="mb-5 rounded-full bg-blue-600 p-1">Typos: {data.user.user.typoCount}</p>
+	<div class="flex">
+		<p class="mb-5 rounded-l-full bg-blue-600 p-1">Typos: {data.user.user.typoCount}</p>
+		<button type="button" class="mb-5 rounded-r-full bg-red-600 hover:bg-red-400 duration-300 p-1 cursor-pointer" onclick={() => isDeleteOpen = !isDeleteOpen}>Delete account</button>
+	</div>
 
 	<p class="mb-3 font-bold">Posts:</p>
 	{#each data.user.posts as post (post.date)}
@@ -18,3 +23,7 @@
 		</div>
 	{/each}
 </div>
+
+{#if isDeleteOpen}
+	<DeleteUserPopup userID={data.user.user.id} />
+{/if}
