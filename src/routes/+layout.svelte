966a4e6f67f8c458e335bd5974eb8475706530c2
thenamelessdev <nameless@thenamelessdev.com>
2026-08-01 14:51:32 +0200

    format: run format cmd

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -12,6 +12,14 @@
 <main class="m-2 flex flex-1 flex-col">
 	{@render children()}
 	<div class="text-center">
-		<p>By using our platform you agree to our <a href="/legal/privacy.pdf" target="_blank" class="underline text-blue-500">Privacy Policy</a> and <a href="/legal/tos.pdf" target="_blank" class="underline text-blue-500">Terms of Service</a></p>
+		<p>
+			By using our platform you agree to our <a
+				href="/legal/privacy.pdf"
+				target="_blank"
+				class="text-blue-500 underline">Privacy Policy</a
+			>
+			and
+			<a href="/legal/tos.pdf" target="_blank" class="text-blue-500 underline">Terms of Service</a>
+		</p>
 	</div>
 </main>
